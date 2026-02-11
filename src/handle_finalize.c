2662e6320327b9e273ac0ad5c882b5bfd22a6a9c
3for <287494524@qq.com>
2026-02-11 18:03:04 +0800

    finalize ok now

--- src/handle_finalize.c
+++ src/handle_finalize.c
@@ -7,15 +7,15 @@ void handle_finalize(ethPluginFinalize_t *msg) {
 
     // EDIT THIS: Set the total number of screen you will need.
     msg->numScreens = 2;
-    // EDIT THIS: Handle this case like you wish to (i.e. maybe no additional screen needed?).
+    /* // EDIT THIS: Handle this case like you wish to (i.e. maybe no additional screen needed?).
     // If the beneficiary is NOT the sender, we will need an additional screen to display it.
     if (memcmp(msg->address, context->beneficiary, ADDRESS_LENGTH) != 0) {
         msg->numScreens += 1;
-    }
+    }*/
 
     // EDIT THIS: set `tokenLookup1` (and maybe `tokenLookup2`) to point to
     // token addresses you will info for (such as decimals, ticker...).
-    msg->tokenLookup1 = context->token_received;
+    msg->tokenLookup1 = context->to_address; // TODO. Not proper here.
 
     msg->result = ETH_PLUGIN_RESULT_OK;
 }
