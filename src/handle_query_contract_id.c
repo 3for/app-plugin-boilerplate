38bf13b1354f652a3314e6dc880dcba7b615d82f
3for <287494524@qq.com>
2026-02-24 17:41:07 +0800

    improve ETH_PLUGIN_QUERY_CONTRACT_ID

--- src/handle_query_contract_id.c
+++ src/handle_query_contract_id.c
@@ -10,6 +10,18 @@ void handle_query_contract_id(ethQueryContractID_t *msg) {
     strlcpy(msg->name, APPNAME, msg->nameLength);
 
     // EDIT THIS: Adapt the cases by modifying the strings you pass to `strlcpy`.
+    switch (context->selectorIndex) {
+        case SWAP_EXACT_ETH_FOR_TOKENS:
+            strlcpy(msg->version, "Swap", msg->versionLength);
+            break;
+        case TRANSFER_TO_VALUE:
+            strlcpy(msg->version, "Transfer", msg->versionLength);
+            break;
+        default:
+            PRINTF("Selector index: %d not supported\n", context->selectorIndex);
+            msg->result = ETH_PLUGIN_RESULT_ERROR;
+            return;
+    }
     if (context->selectorIndex == SWAP_EXACT_ETH_FOR_TOKENS) {
         strlcpy(msg->version, "Swap", msg->versionLength);
         msg->result = ETH_PLUGIN_RESULT_OK;
@@ -17,4 +29,6 @@ void handle_query_contract_id(ethQueryContractID_t *msg) {
         PRINTF("Selector index: %d not supported\n", context->selectorIndex);
         msg->result = ETH_PLUGIN_RESULT_ERROR;
     }
+    // Return valid status.
+    msg->result = ETH_PLUGIN_RESULT_OK;
 }
