e0836c24efec8ea7da6cffef069fc85aaa470a5c
3for <287494524@qq.com>
2026-03-24 18:22:11 +0800

    feat(test): try to improve test_swap_exact_eth_for_token ui

--- src/handle_finalize.c
+++ src/handle_finalize.c
@@ -2,29 +2,53 @@
 
 void handle_finalize(tronPluginFinalize_t *msg) {
     context_t *context = (context_t *) msg->pluginContext;
+    PRINTF("finalize selectorIndex: %d\n", context->selectorIndex);
     if (msg->txContent != NULL) {
         PRINTF("finalize contractAddress(TRON): %.*H\n",
                TRON_ADDRESS_SIZE,
                msg->txContent->contractAddress);
     }
-    PRINTF("finalize to_address(EVM): %.*H\n", ADDRESS_LENGTH, context->to_address);
-
     msg->uiType = TRON_UI_TYPE_GENERIC;
 
-    // EDIT THIS: Set the total number of screen you will need.
-    msg->numScreens = 2;
-    // EDIT THIS: Handle this case like you wish to (i.e. maybe no additional screen needed?).
-    // If the to_address is NOT the contract, we will need an additional screen to display it.
-    // TRON contractAddress is 21 bytes (0x41 + 20-byte EVM payload). Compare only the 20-byte
-    // payload with the parsed EVM `to_address`.
-    if (msg->txContent == NULL ||
-        memcmp(msg->txContent->contractAddress + 1, context->to_address, ADDRESS_LENGTH) != 0) {
-        msg->numScreens += 1;
-    }
+    switch (context->selectorIndex) {
+        case TRANSFER_TO_VALUE:
+            PRINTF("finalize to_address(EVM): %.*H\n", ADDRESS_LENGTH, context->to_address);
+            // EDIT THIS: Set the total number of screen you will need.
+            msg->numScreens = 2;
+            // EDIT THIS: Handle this case like you wish to (i.e. maybe no additional screen needed?).
+            // If the to_address is NOT the contract, we will need an additional screen to display it.
+            // TRON contractAddress is 21 bytes (0x41 + 20-byte EVM payload). Compare only the 20-byte
+            // payload with the parsed EVM `to_address`.
+            if (msg->txContent == NULL ||
+                memcmp(msg->txContent->contractAddress + 1, context->to_address, ADDRESS_LENGTH) != 0) {
+                msg->numScreens += 1;
+            }
 
-    // EDIT THIS: set `tokenLookup1` (and maybe `tokenLookup2`) to point to
-    // token addresses you will info for (such as decimals, ticker...).
-    msg->tokenLookup1 = msg->txContent->contractAddress;
+            // EDIT THIS: set `tokenLookup1` (and maybe `tokenLookup2`) to point to
+            // token addresses you will info for (such as decimals, ticker...).
+            msg->tokenLookup1 = msg->txContent->contractAddress;
+            break;
+        case SWAP_EXACT_ETH_FOR_TOKENS:
+            // EDIT THIS: Set the total number of screen you will need.
+            msg->numScreens = 2;
+            // EDIT THIS: Handle this case like you wish to (i.e. maybe no additional screen needed?).
+            // If the beneficiary is NOT the sender, we will need an additional screen to display it.
+            // `txContent->account` is the TRON owner address (0x41 + 20-byte payload).
+            if (msg->txContent != NULL) {
+                PRINTF("finalize account(TRON): %.*H\n", TRON_ADDRESS_SIZE, msg->txContent->account);
+            }
+            PRINTF("finalize beneficiary: %.*H\n", ADDRESS_LENGTH, context->beneficiary);
+            if (msg->txContent == NULL ||
+                memcmp(msg->txContent->account + 1, context->beneficiary, ADDRESS_LENGTH) != 0) {
+                msg->numScreens += 1;
+            }
+
+            // EDIT THIS: set `tokenLookup1` (and maybe `tokenLookup2`) to point to
+            // token addresses you will info for (such as decimals, ticker...).
+            msg->tokenLookup1 = context->token_received;
+
+            break;
+    }
 
     msg->result = TRON_PLUGIN_RESULT_OK;
 }
--- src/handle_query_contract_ui.c
+++ src/handle_query_contract_ui.c
@@ -70,6 +70,77 @@ static bool set_to_address_ui(ethQueryContractUI_t *msg, context_t *context) {
     return ethToTronBase58(eth_address, msg->msg, msg->msgLength);
 }
 
+// Set UI for the "Send" screen.
+// EDIT THIS: Adapt / remove this function to your needs.
+static bool set_send_ui(ethQueryContractUI_t *msg) {
+    strlcpy(msg->title, "Send", msg->titleLength);
+
+    if (msg->txContent == NULL) {
+        return false;
+    }
+
+    uint8_t amount[sizeof(uint64_t)];
+    uint64_t send_amount = msg->txContent->amount[0];
+
+    for (size_t i = 0; i < sizeof(amount); i++) {
+        amount[sizeof(amount) - 1 - i] = (uint8_t) (send_amount & 0xFF);
+        send_amount >>= 8;
+    }
+
+    // Converts the native TRX amount located in `amount` to its string representation and
+    // copies this to `msg->msg`.
+    return amountToString(amount,
+                          sizeof(amount),
+                          SUN_TO_TRX,
+                          msg->network_ticker,
+                          msg->msg,
+                          msg->msgLength);
+}
+
+// Set UI for "Receive" screen.
+// EDIT THIS: Adapt / remove this function to your needs.
+static bool set_receive_ui(ethQueryContractUI_t *msg, const context_t *context) {
+    strlcpy(msg->title, "Receive Min.", msg->titleLength);
+
+    uint8_t decimals = context->decimals;
+    const char *ticker = context->ticker;
+
+    // If the token look up failed, use the default network ticker along with the default decimals.
+    if (!context->token_found) {
+        decimals = WEI_TO_ETHER;
+        ticker = msg->network_ticker;
+    }
+
+    return amountToString(context->amount_received,
+                          sizeof(context->amount_received),
+                          decimals,
+                          ticker,
+                          msg->msg,
+                          msg->msgLength);
+}
+
+// Set UI for "Beneficiary" screen.
+// EDIT THIS: Adapt / remove this function to your needs.
+static bool set_beneficiary_ui(ethQueryContractUI_t *msg, context_t *context) {
+    strlcpy(msg->title, "Beneficiary", msg->titleLength);
+
+    // Prefix the address with `0x`.
+    msg->msg[0] = '0';
+    msg->msg[1] = 'x';
+
+    // We need a random chainID for legacy reasons with `getEthAddressStringFromBinary`.
+    // Setting it to `0` will make it work with every chainID :)
+    uint64_t chainid = 0;
+
+    // Get the string representation of the address stored in `context->beneficiary`. Put it in
+    // `msg->msg`.
+    return getEthAddressStringFromBinary(
+        context->beneficiary,
+        msg->msg + 2,  // +2 here because we've already prefixed with '0x'.
+        chainid);
+}
+
+
 void handle_query_contract_ui(ethQueryContractUI_t *msg) {
     context_t *context = (context_t *) msg->pluginContext;
     bool ret = false;
@@ -82,19 +153,40 @@ void handle_query_contract_ui(ethQueryContractUI_t *msg) {
     memset(msg->msg, 0, msg->msgLength);
 
     // EDIT THIS: Adapt the cases for the screens you'd like to display.
-    switch (msg->screenIndex) {
-        case 0:
-            ret = set_to_address_ui(msg, context);
+    switch (context->selectorIndex) {
+        case TRANSFER_TO_VALUE:
+            switch (msg->screenIndex) {
+                case 0:
+                    ret = set_to_address_ui(msg, context);
+                    break;
+                case 1:
+                    ret = set_value_ui(msg, context);
+                    break;
+                case 2:
+                    ret = set_contract_ui(msg, context);
+                    break;
+                // Keep this
+                default:
+                    PRINTF("Received an invalid screenIndex\n");
+            }
             break;
-        case 1:
-            ret = set_value_ui(msg, context);
+        case SWAP_EXACT_ETH_FOR_TOKENS:
+            // EDIT THIS: Adapt the cases for the screens you'd like to display.
+            switch (msg->screenIndex) {
+                case 0:
+                    ret = set_send_ui(msg);
+                    break;
+                case 1:
+                    ret = set_receive_ui(msg, context);
+                    break;
+                case 2:
+                    ret = set_beneficiary_ui(msg, context);
+                    break;
+                // Keep this
+                default:
+                    PRINTF("Received an invalid screenIndex\n");
+            }
             break;
-        case 2:
-            ret = set_contract_ui(msg, context);
-            break;
-        // Keep this
-        default:
-            PRINTF("Received an invalid screenIndex\n");
     }
     msg->result = ret ? TRON_PLUGIN_RESULT_OK : TRON_PLUGIN_RESULT_ERROR;
 }
